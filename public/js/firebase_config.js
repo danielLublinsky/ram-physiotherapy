1b5f87d306c76617453ae1306d20bb669884b706
zzzxxxzz <zzzxxxzz123abc@gmail.com>
2024-10-24 15:08:44 +0300

    firebase analytics + footer

--- public/js/firebase_config.js
+++ public/js/firebase_config.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase from the CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCYI4ZSwKTfk4L0uB1Izb9hgJ1ohndsbP4",
   authDomain: "ramphysiotherapy-32d22.firebaseapp.com",
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,14 @@
 (function ($) {
   "use strict";
 
+  document.addEventListener("DOMContentLoaded", function () {
+    fetch("./components/footer.html")
+      .then((response) => response.text())
+      .then((data) => {
+        document.getElementById("footer-placeholder").innerHTML = data;
+      });
+  });
+
   // Dropdown on mouse hover
   $(document).ready(function () {
     function toggleNavbarMethod() {
