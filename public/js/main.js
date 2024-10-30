1b6af66fa22155f525ccb3c4c385653707cf9027
zzzxxxzz <zzzxxxzz123abc@gmail.com>
2024-10-30 23:11:33 +0200

    added dynamic text block

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,49 @@
+function loadTextBlock(targetId, titles, description, imgSrc) {
+  fetch("./components/text-block.html")
+    .then((response) => response.text())
+    .then((template) => {
+      // Inject template into the target element
+      const targetElement = document.getElementById(targetId);
+      targetElement.innerHTML = template;
+
+      // Populate template content dynamically within the specific target element
+      targetElement.querySelector(".template-title-1").textContent = titles[0];
+      targetElement.querySelector(".template-title-2").textContent = titles[1];
+      targetElement.querySelector(".template-img").src = imgSrc;
+
+      const descriptionElement = targetElement.querySelector(".template-description");
+      const readMoreBtn = targetElement.querySelector(".read-more-btn");
+
+      // Character limit for the short version of the description
+      const charLimit = 1000;
+
+      // Set the full description as data attribute
+      descriptionElement.setAttribute("data-full-text", description);
+
+      // If the description exceeds charLimit, show a shortened version and display "Read More" button
+      if (description.length > charLimit) {
+        descriptionElement.textContent = description.substring(0, charLimit) + "...";
+        readMoreBtn.style.display = "inline-block";
+
+        // Add click event for "Read More" button
+        readMoreBtn.addEventListener("click", function () {
+          // Toggle between showing full text and short text
+          if (readMoreBtn.textContent === "קרא עוד") {
+            descriptionElement.textContent = description;
+            readMoreBtn.textContent = "קרא פחות";
+          } else {
+            descriptionElement.textContent = description.substring(0, charLimit) + "...";
+            readMoreBtn.textContent = "קרא עוד";
+          }
+        });
+      } else {
+        // If the description is short, display it in full
+        descriptionElement.textContent = description;
+      }
+    });
+}
+
+
 (function ($) {
   "use strict";
 
